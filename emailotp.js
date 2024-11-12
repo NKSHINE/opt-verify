9fbefa9a79748c73a72568ea98b981a4bb203206
JOEL-JOZEF <neethukshine@gmail.com>
2024-11-12 21:23:54 +0530

    token commit

--- emailotp.js
+++ emailotp.js
@@ -5,9 +5,9 @@ function SendOTP(){
     let otp_code = Math.floor(Math.random() * 10000);
     let emailbody = `<h1> Your OTP is </h1> ${otp_code}`;
     Email.send({
-        SecureToken : "af6e9c76-f18d-42eb-a1ec-bbe91641ab79",
+        SecureToken : "bbad2b8a-9843-4d48-8309-f840effa49b4",
         To : email.value,
-        From : "kashanfaheem88@gmail.com",
+        From : "neethukshine@gmail.com",
         Subject : "Email OTP using Javascript",
         Body : Emailbody,
     }).then(
